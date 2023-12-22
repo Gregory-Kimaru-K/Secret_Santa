c18e6d525a9396e50e5a3fd26a0ae9242bfa3d4b
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2023-12-22 10:39:26 +0300

    me

--- app.py
+++ app.py
@@ -247,5 +247,5 @@ if __name__ == '__main__':
         db.create_all()
         # Enable HTTPS SSl (For testing not production)
 
-        # app.run(debug=False, host="0.0.0.0", port=5000)
-        app.run(debug=True)
+        app.run(debug=False, host="0.0.0.0", port=5000)
+        # app.run(debug=True)
